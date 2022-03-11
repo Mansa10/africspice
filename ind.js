cafb0cd801bc82acd65d0a8cb0c4228f01b2c4ae
Mansa10 <moussa1920@yahoo.fr>
2022-03-10 17:20:29 -0800

    updated server addrr

--- ind.js
+++ ind.js
@@ -230,7 +230,7 @@ fs.appendFile('myMessages.txt', '\nname: ' + name + '\nemail: ' + email + '\nsub
 });
 res.redirect('/contact.html');
 })
-var server = app.listen(3210, function () {  
+var server = app.listen(80, function () {  
   var host = server.address().address  
   var port = server.address().port  
   console.log("Example app listening at http://%s:%s", host, port)  
